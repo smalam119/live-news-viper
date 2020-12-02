6d9013239f88f98291dfff51886a3aa67397758f
Sayed Mahmudul Alam <smalam119@gmail.com>
2020-12-03 00:52:41 +0800

    file structure updated.

--- Live News/modules/entity/LiveNewsModel.swift
+++ Live News/modules/entity/LiveNewsModel.swift
@@ -1,41 +1,18 @@
 //
-//  LiveNewsModel.swift
+//  LiveNewsModelCodable.swift
 //  Live News
 //
-//  Created by MacBook Pro 13" on 1/28/17.
-//  Copyright © 2017 pseudo0. All rights reserved.
+//  Created by Sayed on 01/11/2019.
+//  Copyright © 2019 pseudo0. All rights reserved.
 //
 
 import Foundation
-import ObjectMapper
 
-private let KEY_AUTHOR = "author"
-private let KEY_TITLE = "title"
-private let KEY_DESCRIPTION = "description"
-private let KEY_URL = "url"
-private let KEY_URLTOIMAGE = "urltoimage"
-private let KEY_PUBLISHED_AT = "publishedAt"
-
-
-class LiveNewsModel: Mappable{
-    internal var author: String?
-    internal var title: String?
-    internal var description: String?
-    internal var url: String?
-    internal var urlToImage: String?
-    internal var publishedAt: String?
-    
-    required init?(map: Map) {
-        mapping(map: map)
-    }
-    
-    func mapping(map: Map) {
-        author <- map[KEY_AUTHOR]
-        title <- map[KEY_TITLE]
-        description <- map[KEY_DESCRIPTION]
-        url <- map[KEY_URL]
-        urlToImage <- map[KEY_URLTOIMAGE]
-        publishedAt <- map[KEY_PUBLISHED_AT]
-    }
-    
+struct LiveNewsModel: Codable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
 }
